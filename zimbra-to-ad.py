eac2863ed6db455049256ff0be31ef3f033db2a4
arthur <geexmmo@gmail.com>
2021-05-05 19:23:07 +0600

    some cleanup

--- zimbra-to-ad.py
+++ zimbra-to-ad.py
@@ -1,62 +1,43 @@
 #!/usr/bin/env python
 import re
 
-# mailLists = {'MailList':'','ListEmail':'','CN':'','maillist':[]}
-mailLists = {}
-mailListslen = len(mailLists)
-# tempMemberList = {}
-# testlist = []
-# tempMemberCount = 0
+distGrpData = {}
+distGrpCount = len(distGrpData)
 
-def matchMailList(line):
-    regexMailList = '(?<=^#\sstartNewlist:\s)[\w\s\d]+(?=\smembers)'
-    match = re.search(regexMailList, line)
+# sould match start of new section in dump file
+def matchDistGrp(line):
+    regexDistGrp = '(?<=^#\sstartNewlist:\s)[\w\s\d]+(?=\smembers)'
+    match = re.search(regexDistGrp, line)
     if match:
         # accessing variables and structures
-        global mailListslen
-        # global tempMemberCount
-        # global tempMemberList
-        # global testlist
-        mailListslen = len(mailLists)
-        # if tempMemberList:
-        #     print('last members', tempMemberList)
+        global distGrpCount
+        distGrpCount = len(distGrpData)
         print('Processing group', match.group())
-        # first 'MailList' added to data structure
-        mailLists[mailListslen] = {'MailList': match.group()}
-        mailLists[mailListslen]['maillist'] = []
-        # resetting member list for new iteration
-        # tempMemberCount = 0
-        # tempMemberList = {} 
+        distGrpData[distGrpCount] = {'MailList': match.group()}
+        # 
+        distGrpData[distGrpCount]['members'] = []
 
-    regexMailListEmail = '(?<=\w\W\s)\S{1,}@\S{2,}\.\S{2,}$'
-    match = re.search(regexMailListEmail, line)
+    regexDistGrpEmail = '(?<=\w\W\s)\S{1,}@\S{2,}\.\S{2,}$'
+    match = re.search(regexDistGrpEmail, line)
     if match:
         print(match.group())
-        mailLists[mailListslen]['ListEmail'] = match.group()
+        distGrpData[distGrpCount]['ListEmail'] = match.group()
 
     regexCN = '(?<=^cn:\s)[\w\s\d]+$'
     match = re.search(regexCN, line)
     if match:
-        mailLists[mailListslen]['CN'] = match.group()
+        distGrpData[distGrpCount]['CN'] = match.group()
 
     regexMemberMail = '^\S{1,}@\S{2,}\.\S{2,}$'
     match = re.search(regexMemberMail, line)
     if match:
-        # print('memlist', tempMemberList)
-        # print('memcount', tempMemberCount)
-        # print('maillist', mailLists)
-        # print('maillistlen', mailListslen)
-        mailLists[mailListslen]['maillist'].append(match.group())
-        # tempMemberList[tempMemberCount]['maillist'].append(match.group())
-        # testlist.append(match.group())
-        # tempMemberCount += 1
+        distGrpData[distGrpCount]['members'].append(match.group())
 
 with open('zimbra-test.txt', 'r') as textfile:
     for line in textfile:
         line = line.strip()
-        matchMailList(line)
+        matchDistGrp(line)
 
-# mailLists[mailListslen] = {'MailList': match.group()}
-print(mailLists)
-for i in mailLists:
-    print('test', mailLists[i].get('maillist'))
+print(distGrpData)
+for i in distGrpData:
+    print('test', distGrpData[i].get('members'))
